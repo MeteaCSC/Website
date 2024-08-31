80bd8e6de7eb494a1b7607cd992c4fcded7d7562
David C <theinvisiblebench2@gmail.com>
2024-08-31 00:53:44 -0500

    moved to typescript for greater flexibility

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import express, { Express, Request, Response } from "express";
+
 const marked = require("marked");
 const path = require("node:path");
-const express = require("express");
 const fs = require("node:fs");
 
 const app = express();
@@ -11,7 +12,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 /* loader? */
-function load_md(file) {
+function load_md(file: String) {
   let data = fs.readFileSync(path.join(__dirname, "pages/", file), "utf8");
   return marked.parse(data);
 }
@@ -30,5 +31,5 @@ app.get("/calendar", (req, res) => {
 });
 
 let server = app.listen(7777, () => {
-  console.log("Started express server on port " + server.address().port);
+  console.log("Started express server on", server.address());
 });
